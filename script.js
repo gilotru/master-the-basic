1bdbef4b62dcbcdbf9e863ff398da97d56d6574f
gilotru <delacruzangelo686@gmail.com>
2022-06-06 16:42:55 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+function goldenMiddle(a, b) {
+    console.log([a[1],b[1]]);
+   }
+   goldenMiddle([1,6,8], [4,6,2]); // output [6,6]
+*/
+
+
+/*
+var polidrom = "I LOVE YOU";
+var lowerlove = polidrom.toLowerCase();
+var reverse = "";
+
+for(var change = lowerlove.length - 1; change>=0; change--){
+    reverse += lowerlove.charAt(change);
+    console.log(reverse);
+}
+*/
+
+// password checker
+/*
+let mustpassword = 20;
+function checkPassword(password, password_repeat) {
+  if(password == password_repeat){
+    if(password.length >= 20){
+      return true;
+    }else{
+      return false;
+    }
+  }else{
+    return false;
+  }
+}
+*/
