d8845996cfca36effb963b337b0e5860d690a995
gilotru <delacruzangelo686@gmail.com>
2022-06-09 11:30:02 +0800

    use regexp

--- script.js
+++ script.js
@@ -32,3 +32,36 @@ function checkPassword(password, password_repeat) {
   }
 }
 */
+
+//Check if the password is secure:
+/*
+function checkPassword(password, password_repeat) {
+  const eightchar = new RegExp('(?=^.{8,}$)');
+	const lowercase = new RegExp('(?=.*[a-z])');
+  const uppercase = new RegExp('(?=.*[A-Z])');
+  const number = new RegExp('(?=.*[0-9])');
+  const specialchar = new RegExp('(?=.*[^A-Za-z0-9])');
+  
+  if(password == password_repeat){
+    if(eightchar.test(password)){
+      if(lowercase.test(password)){
+        if(uppercase.test(password)){
+          if(number.test(password)){
+            if(specialchar.test(password)){
+              console.log("log-in");
+            }
+            return false;
+          }
+          return false;
+        }
+        return false;
+      }
+      return false;
+    }
+    return false;
+  }
+  return false;
+}
+
+checkPassword('YY&glk4Hfi_ffS', 'YY&glk4Hfi_ffS');
+*/
